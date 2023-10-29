9d0cacbd218474ac122b7f749574c25f5299a0f9
Srikar charan <srikarsuri@gmail.com>
2023-10-29 14:45:50 +0530

    with creds

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var AWS = require("aws-sdk");
-var S3 = new AWS.S3();
+var S3 = new AWS.S3({
+  accessKeyId: "AKIAYDL5INNMUYTFRRXF",
+  secretAccessKey: "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",
+});
 
 const port = process.env.PORT || 3000,
   http = require("http"),
@@ -8,13 +11,18 @@ const port = process.env.PORT || 3000,
 
 const server = http.createServer(function (req, res) {
   S3.listBuckets(function (err, data) {
+    console.log("in-1");
     var bucketList = "";
     if (err) bucketList = JSON.stringify(err); // an error occurred
     else {
+      console.log("in-2");
+      // successful response
       for (var a = 0; a < data.Buckets.length; a++) {
         bucketList += JSON.stringify(data.Buckets[a].Name);
       }
+      console.log(bucketList);
     }
+    console.log("in-4");
     res.writeHead(200);
     res.write(bucketList);
     res.end();
