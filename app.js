dc1f6e41491cdfd54d053fac5a3f36f35e645a86
Srikar charan <srikarsuri@gmail.com>
2023-10-29 16:31:41 +0530

    Change to v3

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
-var AWS = require("aws-sdk");
-var S3 = new AWS.S3();
+var { S3Client, ListBucketsCommand } = require("@aws-sdk/client-s3");
+var S3 = new S3Client();
+var listBucketsCommand = new ListBucketsCommand({});
 
 const port = process.env.PORT || 3000,
   http = require("http"),
   fs = require("fs"),
   html = fs.readFileSync("index.html");
 
-const server = http.createServer(function (req, res) {
-  S3.listBuckets(function (err, data) {
-    console.log("in-1");
-    var bucketList = "";
-    if (err) bucketList = JSON.stringify(err); // an error occurred
-    else {
-      console.log("in-2");
-      // successful response
-      for (var a = 0; a < data.Buckets.length; a++) {
-        bucketList += JSON.stringify(data.Buckets[a].Name);
-      }
-      console.log(bucketList);
-    }
-    console.log("in-4");
-    res.writeHead(200);
-    res.write(bucketList);
-    res.end();
-  });
+const server = http.createServer(async function (req, res) {
+  const { Owner, Buckets } = await S3.send(listBucketsCommand);
+  console.log(
+    `${Owner.DisplayName} owns ${Buckets.length} bucket${
+      Buckets.length === 1 ? "" : "s"
+    }:`
+  );
+  res.write(`${Buckets.map((b) => ` • ${b.Name}`).join("\n")}`);
+  res.end();
 });
 
 // Listen on port 3000, IP defaults to 127.0.0.1
